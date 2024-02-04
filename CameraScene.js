6f6a270f8d0012aeb7e5b19f535c6a8d9f7599ed
Andrew Chon <andrewyhchon@gmail.com>
2024-02-03 16:37:55 -0800

    updated to start more centered on slime

--- CameraScene.js
+++ CameraScene.js
@@ -2,17 +2,17 @@ class CameraScene {
     constructor(game) {
         this.game = game;
         this.game.camera = this;
+       
         this.x = 0;
         this.y = 0;
     
-       
     };
 
     update() {
         let midpoint = PARAMS.CANVAS_WIDTH/2 - PARAMS.BLOCKWIDTH / 2;
 
-        this.x = this.game.slime.x - midpoint;
-        this.y = this.game.slime.y - midpoint;
+        this.x = Math.max(0, Math.min(this.game.slime.x - midpoint));
+        this.y = Math.max(0, Math.min(this.game.slime.y - midpoint));
 
     };
     
