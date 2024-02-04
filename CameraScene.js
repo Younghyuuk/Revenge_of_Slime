355806819b60b44d51b7135af9ca7aaec22c9183
Andrew Chon <andrewyhchon@gmail.com>
2024-02-03 16:40:53 -0800

    centered and camera is finished

--- CameraScene.js
+++ CameraScene.js
@@ -9,10 +9,13 @@ class CameraScene {
     };
 
     update() {
-        let midpoint = PARAMS.CANVAS_WIDTH/2 - PARAMS.BLOCKWIDTH / 2;
+        // let midpoint = PARAMS.CANVAS_WIDTH/2 - PARAMS.BLOCKWIDTH / 2;
 
-        this.x = Math.max(0, Math.min(this.game.slime.x - midpoint));
-        this.y = Math.max(0, Math.min(this.game.slime.y - midpoint));
+        let midPointX = PARAMS.CANVAS_WIDTH / 2;
+        let midPointY = PARAMS.CANVAS_HEIGHT / 2;
+
+        this.x = Math.max(0, Math.min(this.game.slime.x - midPointX));
+        this.y = Math.max(0, Math.min(this.game.slime.y - midPointY));
 
     };
     
