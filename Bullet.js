ef273a5923b0770c16c41caa371f99ed162cc0a3
Andrew Chon <andrewyhchon@gmail.com>
2024-02-14 00:36:01 -0800

    made bullet and projectile closer to my knife class

--- Bullet.js
+++ Bullet.js
@@ -1,39 +1,18 @@
 class Bullet extends Projectile {
-    constructor(game, x, y, target) {
-        super(game, x, y, target);
-        // Set any Bullet-specific properties
-        this.radius = 5; // Smaller radius for a bullet
-        this.game.bullet = this;
-        this.damage = 10; // Set the damage for a bullet
-        // Override or extend other properties or methods specific to a Bullet
-    }
-
-    // You can override methods or add new ones specific to Bullet
-    update() {
-        // this.heatSeeking = document.getElementById("heatseeking").checked;
-        this.smooth = document.getElementById("smooth").checked;
-
-        // if (this.heatSeeking) {
-        var dist = distance(this, this.target);
-        this.velocity = { x: (this.target.x - this.x) / dist * this.maxSpeed, y: (this.target.y - this.y) / dist * this.maxSpeed };
-        // }
-        
-        this.x += this.velocity.x * this.game.clockTick;
-        this.y += this.velocity.y * this.game.clockTick;
-        
-        for (var i = 0; i < this.game.entities.length; i++) {
-            var ent = this.game.entities[i];
-            if ((ent instanceof enemyArcher || ent instanceof enemyKnight) && this.game.slime.circleIntersect(this, ent)) {
-                ent.hitpoints -= this.damage;
-                this.removeFromWorld = true;
-            }
-        }
-        
-        this.facing = getFacing(this.velocity);
+    constructor(game, x, y, mouseX, mouseY, speed) {
+        const angle = Math.atan2(mouseY - y, mouseX - x);
+        super(game, x, y, angle, speed, BULLET_DESPAWN_TIME);
+        this.damage = 10;
     };
 
-    draw(ctx) {
-        // Custom drawing code for a Bullet, or call the parent draw
-        super.draw(ctx);
-    }
+    update() {
+        super.update();
+        // Check for collisions with game entities
+        this.game.entities.forEach(entity => {
+            if (entity instanceof Slime && this.bc.collidesWith(entity.bc)) {
+                entity.takeDamage(this.damage);
+                this.removeFromWorld = true;
+            }
+        });
+    };
 }
--- Projectile.js
+++ Projectile.js
@@ -1,97 +1,42 @@
 
 class Projectile {
-    constructor(game, x, y, target, fireRate ) {
-        Object.assign(this, { game, x, y, target, fireRate });
-        this.radius = 12;
-        this.smooth = false;
-
-        // this.spritesheet = ASSET_MANAGER.getAsset("./sprites/arrow.png");
-
-        var dist = distance(this, this.target);
-        this.maxSpeed = 200; // pixels per second
-
-        this.velocity = { x: (this.target.x - this.x) / dist * this.maxSpeed, y: (this.target.y - this.y) / dist * this.maxSpeed };
-
-        this.cache = [];
-
-
-        this.animations = [];
-        // this.animations.push(new Animator(this.spritesheet, 0, 0, 32, 32, 1, 0.2, 0, false, true));
-        // this.animations.push(new Animator(this.spritesheet, 40, 0, 32, 32, 1, 0.2, 0, false, true));
-        // this.animations.push(new Animator(this.spritesheet, 80, 0, 32, 32, 1, 0.2, 0, false, true));
-        // this.animations.push(new Animator(this.spritesheet, 120, 0, 32, 32, 1, 0.2, 0, false, true));
-        // this.animations.push(new Animator(this.spritesheet, 160, 0, 32, 32, 1, 0.2, 0, false, true));
-
-        this.facing = 5;
-        
-
-        this.elapsedTime = 0;
-    };
-
-    drawAngle(ctx, angle) {
-        if (angle < 0 || angle > 359) return;
-
-
-        if (!this.cache[angle]) {
-           let radians = angle / 360 * 2 * Math.PI;
-           let offscreenCanvas = document.createElement('canvas');
-
-            offscreenCanvas.width = 32;
-            offscreenCanvas.height = 32;
-
-            let offscreenCtx = offscreenCanvas.getContext('2d');
-
-            offscreenCtx.save();
-            offscreenCtx.translate(16, 16);
-            offscreenCtx.rotate(radians);
-            offscreenCtx.translate(-16, -16);
-            offscreenCtx.drawImage(this.spritesheet, 80, 0, 32, 32, 0, 0, 32, 32);
-            offscreenCtx.restore();
-            this.cache[angle] = offscreenCanvas;
-        }
-        var xOffset = 16;
-        var yOffset = 16;
-
-        ctx.drawImage(this.cache[angle], this.x - xOffset, this.y - yOffset);
-        if (PARAMS.DEBUG) {
-            ctx.strokeStyle = 'Green';
-            ctx.strokeRect(this.x - xOffset, this.y - yOffset, 32, 32);
-        }
+    constructor(game, x, y, angle, speed, despawnTime) {
+        this.game = game;
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+        this.angle = angle;
+        this.radiusZ = 12;
+        this.despawnTime = despawnTime;
+        this.velocityX = Math.cos(this.angle) * this.speed;
+        this.velocityY = Math.sin(this.angle) * this.speed;
+        this.collisionCircle = {x: this.x, y: this.y, radius: this.radiusZ};
     };
 
     update() {
-
+        this.x += this.velocityX * this.game.clockTick;
+        this.y += this.velocityY * this.game.clockTick;
+        this.despawnTime -= this.game.clockTick;
+        if (this.despawnTime <= 0 || this.isOutOfBounds()) {
+            this.removeFromWorld = true;
+        }
     };
 
     draw(ctx) {
-        var xOffset = 16;
-        var yOffset = 16;
-        if (this.smooth) {
-            let angle = Math.atan2(this.velocity.y , this.velocity.x);
-            if (angle < 0) angle += Math.PI * 2;
-            let degrees = Math.floor(angle / Math.PI / 2 * 360);
-
-            this.drawAngle(ctx, degrees);
-        } else {
-            if (this.facing < 5) {
-                this.animations[this.facing].drawFrame(this.game.clockTick, ctx, this.x - xOffset, this.y - yOffset, 1);
-            } else {
-                ctx.save();
-                ctx.scale(-1, 1);
-                this.animations[8 - this.facing].drawFrame(this.game.clockTick, ctx, -(this.x) - 32 + xOffset, this.y - yOffset, 1);
-                ctx.restore();
-            }
-        }
-
-        if (PARAMS.DEBUG) {
-            ctx.strokeStyle = "Red";
-            ctx.beginPath();
-            ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
-            ctx.closePath();
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radiusZ, 0, Math.PI * 2);
+        ctx.fillStyle = 'black'; // Set bullet color
+        ctx.fill();
+        // Optional: Draw collision circle in debug mode
+        if (this.game.debug) {
+            ctx.strokeStyle = 'red';
             ctx.stroke();
         }
     };
-   
+
+    isOutOfBounds() {
+        return this.x < 0 || this.x > this.game.width || this.y < 0 || this.y > this.game.height;
+    };
 };
 
 
